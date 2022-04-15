a66baae1d03cb6f69d8f62ec4497c60a287f9f0d
Alireza Talaei <alireza@Alirezas-MacBook-Pro.local>
2022-04-15 13:25:02 +1000

    project works

--- app/controllers/agents_controller.rb
+++ app/controllers/agents_controller.rb
@@ -11,7 +11,7 @@ class AgentsController < ApplicationController
 
   def create
     agent = Agent.create agent_params    
-    redirect_to login_path
+    redirect_to root_path
   end
 
   def edit
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,6 +29,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :family, :email, :password, :password_confirmation)
   end
 end
